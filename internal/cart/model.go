8ac477170a0374d8eec818ddf83463cf8c934c6a
Austoin <liuzhigui_2023@qq.com>
2026-03-25 01:25:40 +0800

    fix: bind cart request fields correctly

--- internal/cart/model.go
+++ internal/cart/model.go
@@ -1,12 +1,12 @@
 package cart
 
 type Item struct {
-	UserID      uint64
-	ProductID   uint64
-	ProductName string
-	Price       uint64
-	Quantity    int
-	Checked     bool
+	UserID      uint64 `json:"user_id"`
+	ProductID   uint64 `json:"product_id"`
+	ProductName string `json:"product_name"`
+	Price       uint64 `json:"price"`
+	Quantity    int    `json:"quantity"`
+	Checked     bool   `json:"checked"`
 }
 
 func (i Item) IsValidQuantity() bool {
